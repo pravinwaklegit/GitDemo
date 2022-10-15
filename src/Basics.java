214ea2831511b0bda78bce21f2e4d034ba4dd11f
pravinwaklegit <pravin.wakle223@gmail.com>
2022-10-15 16:34:03 +0530

    this is commit from gitdemo asian time

--- src/Basics.java
+++ src/Basics.java
@@ -3,50 +3,31 @@ import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+
 public class Basics {
 
 	public static void main(String[] args) {
-	
-		
-		RestAssured.baseURI="https://rahulshettyacademy.com";
-		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type" , "application/json")
-		.body("{\r\n"
-				+ "  \"location\": {\r\n"
-				+ "    \"lat\": -38.383494,\r\n"
-				+ "    \"lng\": 33.427362\r\n"
-				+ "  },\r\n"
-				+ "  \"accuracy\": 50,\r\n"
-				+ "  \"name\": \"Frontline house\",\r\n"
-				+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
-				+ "  \"address\": \"29, side layout, cohen 09\",\r\n"
-				+ "  \"types\": [\r\n"
-				+ "    \"shoe park\",\r\n"
-				+ "    \"shop\"\r\n"
-				+ "  ],\r\n"
-				+ "  \"website\": \"http://google.com\",\r\n"
-				+ "  \"language\": \"French-IN\"\r\n"
-				+ "}").when().post("maps/api/place/add/json")
-			.then().log().all().assertThat().statusCode(200	).body("scope", equalTo("APP"))
-			.header("server", "Apache/2.4.41 (Ubuntu)").extract().response().asString();
-			
-		
-			System.out.println(response);
-			JsonPath js= JsonPath(response);
-			String placeId = js.getString("place_id");
-			
-			System.out.println(placeId);
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
+
+		RestAssured.baseURI = "https://rahulshettyacademy.com";
+		String response = given().log().all().queryParam("key", "qaclick123").header("Content-Type", "application/json")
+				.body("{\r\n" + "  \"location\": {\r\n" + "    \"lat\": -38.383494,\r\n" + "    \"lng\": 33.427362\r\n"
+						+ "  },\r\n" + "  \"accuracy\": 50,\r\n" + "  \"name\": \"Frontline house\",\r\n"
+						+ "  \"phone_number\": \"(+91) 983 893 3937\",\r\n"
+						+ "  \"address\": \"29, side layout, cohen 09\",\r\n" + "  \"types\": [\r\n"
+						+ "    \"shoe park\",\r\n" + "    \"shop\"\r\n" + "  ],\r\n"
+						+ "  \"website\": \"http://google.com\",\r\n" + "  \"language\": \"French-IN\"\r\n" + "}")
+				.when().post("maps/api/place/add/json").then().log().all().assertThat().statusCode(200)
+				.body("scope", equalTo("APP")).header("server", "Apache/2.4.41 (Ubuntu)").extract().response()
+				.asString();
+
+		System.out.println(response);
+		System.out.println("Hello1");
+		System.out.println("Hello2");
+		JsonPath js = JsonPath(response);
+		String placeId = js.getString("place_id");
+
+		System.out.println(placeId);
+
 	}
 
 	private static JsonPath JsonPath(String response) {
